81117ea6c10224ea9c9141b258b6ea0e637e561e
Jake <jakevanhout@outlook.com>
2022-05-16 19:44:51 +0200

    add event models for user events

---
+++ user-service/Models/Events/AddUserEvent.cs
@@ -0,0 +1,7 @@
+﻿namespace user_service.Models.Events;
+
+public class AddUserEvent
+{
+    public string Id { get; set; } = "";
+    public string DisplayName { get; set; } = "";
+}
---
+++ user-service/Models/Events/UpdateUserEvent.cs
@@ -0,0 +1,9 @@
+﻿namespace user_service.Models.Events;
+
+public class UpdateUserEvent
+{
+    public string Id { get; set; } = "";
+    public string DisplayName { get; set; } = "";
+
+    public string Bio { get; set; } = "";
+}
